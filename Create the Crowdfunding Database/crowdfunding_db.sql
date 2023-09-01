f11ccb3be519782f4de5d8a404817fe250dccaf6
Sergio Escobedo <sergioescobedo@MacBook-Pro-de-Sergio.local>
2023-08-31 19:29:07 -0600

    adding a new python file

---
+++ Create the Crowdfunding Database/crowdfunding_db.sql
@@ -0,0 +1,11 @@
+-- Verify Contacts Table
+SELECT * FROM Contacts;
+
+-- Verify Category Table
+SELECT * FROM Category;
+
+-- Verify Subcategory Table
+SELECT * FROM Subcategory;
+
+-- Verify Campaign Table
+SELECT * FROM Campaign;
